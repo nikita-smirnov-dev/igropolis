c06fee08e4844f4f9ec0a98a8f5160dec32c4345
Nikita Smirnov <nikita_smirnov91@mail.ru>
2026-03-16 18:24:26 +0300

    added component MainCollections

---
+++ src/assets/js/components/main-collections-slider.js
@@ -0,0 +1,29 @@
+import Swiper from "swiper";
+import { Navigation } from "swiper";
+import { getGap } from "@/assets/js/helpers/get-gap";
+
+const initMainCollectionsSlider = () => {
+  const slider = document.querySelector("[data-main-collections-slider]");
+
+  if (!slider) return;
+
+  const gap = getGap();
+
+  new Swiper(slider, {
+    modules: [Navigation],
+    slidesPerView: 2,
+    spaceBetween: gap,
+    speed: 700,
+    loop: true,
+    loopAdditionalSlides: 1,
+    a11y: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: ".main-collections__slider-btn--next",
+      prevEl: ".main-collections__slider-btn--prev",
+    },
+  });
+};
+
+initMainCollectionsSlider();
